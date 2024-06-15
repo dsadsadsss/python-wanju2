13b1280f14119c46f64263aef2023705131b67bb
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2024-06-15 18:29:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
-import shlex
 from flask import Flask
+from multiprocessing import Process
 
 # Function to start the web server
 def start_server(port):
@@ -14,14 +14,17 @@ def start_server(port):
     app.run(host='0.0.0.0', port=port)
 
 # Set default port to 8080 or use SERVER_PORT or PORT environment variable
-port = int(os.environ.get('SERVER_PORT', os.environ.get('PORT', 8080)))
+port = int(os.environ.get('SERVER_PORT', os.environ.get('PORT', 3000)))
 
 # Define the command to be executed
 cmd = "chmod +x ./start.sh && ./start.sh"
 
-# Start the web server in a separate process group
-web_server_command = ['python', __file__, '--start-server', str(port)]
-subprocess.Popen(web_server_command, preexec_fn=os.setsid)
+# Start the web server in a separate process
+server_process = Process(target=start_server, args=(port,))
+server_process.start()
 
-# Start the web server
-start_server(port)
+# Execute the shell command with shell=True
+subprocess.run(cmd, shell=True)
+
+# Optionally, join the server process if you want the script to wait for the server to finish
+server_process.join()
