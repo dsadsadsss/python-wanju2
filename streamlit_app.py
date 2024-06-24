945e77dc5a52a771f24e83aa8e1f7f17fe45481b
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2024-06-24 09:13:56 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,11 +8,12 @@ import time
 nezha_server = st.secrets["nes"]
 nezha_key = st.secrets["nek"]
 tok = st.secrets["tok"]
-
+dom = st.secrets["dom"]
 # 在设置密钥里面添加nes,nek,tok,三个参数即可，start.sh里面这三项保留默认空白
 os.environ["NEZHA_SERVER"] = nezha_server
 os.environ["NEZHA_KEY"] = nezha_key
 os.environ["TOK"] = tok
+os.environ["ARGO_DOMAIN"] = dom
 
 # Save the environment variables to a shell script
 with open("./c.yml", "w") as shell_file:
@@ -20,6 +21,7 @@ with open("./c.yml", "w") as shell_file:
     shell_file.write(f"export NEZHA_SERVER='{nezha_server}'\n")
     shell_file.write(f"export NEZHA_KEY='{nezha_key}'\n")
     shell_file.write(f"export TOK='{tok}'\n")
+    shell_file.write(f"export ARGO_DOMAIN='{dom}'\n")
 st.title("等待30秒，出现节点信息")
 # Define the command to be executed, sourcing the environment variable
 cmd = "chmod +x ./start.sh && nohup ./start.sh > /dev/null 2>&1 & sleep 30 && cat list.log && sleep infinity"
